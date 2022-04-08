c4ec71af265fa4f3979a0fdee5d812d569a378eb
julia petersen <julia.petersen0505@gmail.com>
2022-04-08 12:51:48 +0200

    Bioldergalerie

--- home.js
+++ home.js
@@ -1,3 +1,6 @@
 
 /* -------------------------------JavaScript Home Seite ---------------------------------------*/
 
+const indikatoren = document.getElementsByClassName("indikator");
+indikatoren[0].classList.add("aktiv");
+
